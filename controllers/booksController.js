0268d1f1202831326a0de4db4bd94cb94f142717
Cadin McQueen <cadin.mcqueen@gmail.com>
2019-01-05 12:57:11 -0500

    Remove console.logs

--- controllers/booksController.js
+++ controllers/booksController.js
@@ -28,10 +28,8 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   remove: function(req, res) {
-    // console.log(req)
     db.Book
       .findOneAndRemove({ id: req.params.id })
-      // .then(dbModel => dbModel.remove())
       .then(dbModel => res.json(dbModel))
       .catch(err => {
         console.log(err);
